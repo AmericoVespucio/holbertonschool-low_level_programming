ead28a0808225685096d5456ccd79b45bcfaf3a0
AmericoVespucio <estebanaguiarx@gmail.com>
2020-09-17 21:42:33 +0000

    3.2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,11 @@ int main(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-	}
+
 	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(d);
-	}
+		putchar(c);
+
 	putchar('\n');
 	return (0);
 }
