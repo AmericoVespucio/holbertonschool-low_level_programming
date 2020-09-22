99deceb14499431c7884a5221d6d16be92218635
AmericoVespucio <estebanaguiarx@gmail.com>
2020-09-22 16:05:17 +0000

    7.8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-
 /**
  * print_last_digit - Entry point
  * @n: int
- * Return: lastdigit (Success)
+ * Return: last digit (Success)
  */
-
 int print_last_digit(int n)
 {
-int lastdigit;
-if (n < 0)
-{
-n = (n * -1);
-}
-lastdigit = n % 10;
-_putchar(lastdigit);
-return (lastdigit);
+	int LastD = n % 10;
+
+	if (n < 0)
+	{
+		LastD = LastD * -1;
+	}
+	_putchar(LastD + '0');
+	return (LastD);
 }
