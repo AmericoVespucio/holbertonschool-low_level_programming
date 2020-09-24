d6592d6a152d0a036b236ce3cff8d3658e3583d5
AmericoVespucio <estebanaguiarx@gmail.com>
2020-09-24 18:22:35 +0000

    6.2

---
+++ 0x04-more_functions_nested_loops/.#holberton.h
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.3005:1600967148
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: line
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	if (n < 0)
+	{
+		for (n = n; n > 0; a--)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+}
