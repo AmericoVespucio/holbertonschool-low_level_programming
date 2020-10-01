67ac4f8afad455490774fea57a13fde4f7397562
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-01 09:09:43 -0700

    1.2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,14 +4,14 @@
  * _strcat - check the code for Holberton School students.
  * @dest: destination
  * @src: source
+ * @n: int
  * Return: Always 0.
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	int n;
 
 	for (i = 0; dest[i] != '\0'; ++i)
 	{
