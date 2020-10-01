e680aacc69b828c6047d7eff2d7ef69f7e254413
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-01 08:52:47 -0700

    0.1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,14 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	int largo;
 
 	for (i = 0; dest[i] != '\0'; ++i)
 	{
 	}
-	for (j = 0; src[j] != '\0'; ++j)
-	{
-	}
-	for (largo = 0; largo != i; ++largo)
+	for (j = 0; src[j] != '\0'; ++j, ++i)
 	{
 		dest[i] = src[j];
-		++largo;
 	}
-	dest[largo + 1] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
