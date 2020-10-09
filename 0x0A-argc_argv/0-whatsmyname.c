9e2a0decf25a92f77a8e39a9922f5baab39a8cf9
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-09 12:05:05 -0700

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - taking in the input that you would normally use to execute the file
+ *and return that as an output
+ *@argc: Variable that accounts for the length of the input
+ *@argv: Variable that accounts for the length of the input as an array
+ *Return: The value returns 0
+ */
+
+int main(int argc, char* argv[])
+{
+	if(argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return(0);
+}
