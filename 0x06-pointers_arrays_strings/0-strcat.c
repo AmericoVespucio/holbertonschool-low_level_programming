daeb69b84a0e7037aa7ede987ebb7b196a9abf46
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-10-12 18:13:59 -0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,13 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-
-	for (i = 0; src[i] != '\0'; ++j, ++i)
+	for (i = 0; dest[i] != '\0'; ++i)
 	{
-		dest[j] = src[i];
 	}
-	dest[j] = '\0';
+	for (j = 0; src[j] != '\0'; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
 	return (dest);
 }
