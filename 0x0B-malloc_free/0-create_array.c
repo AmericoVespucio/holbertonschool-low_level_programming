8ebeb9797fed0db13ac2ab7fa8388e95333a1d60
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-14 08:01:35 -0700

    Libraries, Readme, Arrays

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array: creates an array of strings
+ * @size: size of the array
+ * @c: chars
+ *
+ * return: the string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc (( size + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	if ( s == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	s[i] = '\0';
+	return(s);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char *copystr(char *dest, char *source);
+char **strtow(char *str);
+char **countwords(char *s);
+void fillarr(char **s1, char *s);
+char **countletters(char *s, char **q);
+#endif /* _SAMPLE_HEADER_H_ */
