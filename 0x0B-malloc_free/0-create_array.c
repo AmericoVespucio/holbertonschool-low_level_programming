d686eaa36f5611ae58affc3b616b4845330243ec
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-14 09:27:38 -0700

    Array 202

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	if (s == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
@@ -28,6 +28,6 @@ char *create_array(unsigned int size, char c)
 	{
 		s[i] = c;
 	}
-	s[i] = '\0';
+
 	return (s);
 }
