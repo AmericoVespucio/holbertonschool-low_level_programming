0ba0c24ba48415f9e6e2a08064b396eb45f60126
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-14 11:03:23 -0700

    Strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,31 +9,32 @@
 
 char *_strdup(char *str)
 {
-	char *s;
-	int i;
-	int size = 0;
+	char *s; /** Nuevo puntero, para usar malloc */
+	int i; /** Indexador */
+	int size = 0; /** Toma el valor del primer loop y lo guarda en size*/
 
-	if (str == NULL)
+	if (str == NULL) /** Checkea si str es null, si no es, continua */
 	{
 		return (NULL);
 	}
 
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != 0; i++) /** Cuenta lugares del string s */
 	{
-		size++;
+		size++; /** Almacena lugares en size */
 	}
 
 	s = malloc(sizeof(char) * size + 1);
-
+	/** Iguala s a la alocacion de memoria malloc, +1 para contar el null */
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
+	/** Si el nuevo puntero resulta en un null, return null */
 	for (i = 0; i < size; i++)
 	{
 		s[i] = str[i];
 	}
-
+/** cuenta hasta size, copia el contenido de str a nuevo puntero en */
+/** la nueva direccion de memoria */
 	return (s);
 }
