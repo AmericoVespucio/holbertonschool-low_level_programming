17fa5ee08c1553a9ca649f0f8ea329dfb513b972
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-19 20:51:47 +0000

    Init Dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,8 +14,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = &dog;
-	d->name;
-	d->age;
-	d->owner;
+	if (d != NULL) /* If d is not NULL */
+	{ /* We derreference d pointer to name             */
+	  /* and we asign the value of name to the pointer */
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+/* We also do it with age and owner */
+	}
 }
