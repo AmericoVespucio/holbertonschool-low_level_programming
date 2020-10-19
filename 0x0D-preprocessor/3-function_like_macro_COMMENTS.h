adbe36f3eca46b595ee6e9e0f111a4536dfad020
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-19 19:01:08 +0000

    Function Like Macro

--- 0x0D-preprocessor/3-function_like_macro_COMMENTS.h
+++ 0x0D-preprocessor/3-function_like_macro_COMMENTS.h
@@ -1,10 +1,6 @@
-#ifndef ABS
-#define ABS(X) ((X<0)?(-X):(X))
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+/** x is lesser than 0?, if its, then change the sign of x to - (- x - = +), else, x */
+#define ABS(x) ((x < 0) ? -(x) : (x))
+
 #endif
-/*
- * Macro Arguments:
- * ? - if, in this case: if x less than 0 then change X to -X
- * : - else, in this case, if x is not lesser than 0, then show X value
- * Why?: Absolute value is always positive, if its negative, the -x its
- * gonna change the sign to positive
- */
