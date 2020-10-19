5f71307852f0a5f68e34712c0e8c2dae11ec961e
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-10-19 19:50:34 -0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * _strlen - check the code for Holberton School students.
- * @s: int pinter to be determined.
- * Return: 0 or 1.
+ * _strlen - Check the lenght of a desired string
+ * @s: String, while i will take the value of the last string position
+ * Return: The lenght of the string
  */
  
  	int _strlen(char *s)
@@ -19,9 +19,9 @@
 	}
 
 /**
- * _strdup - check the code for Holberton School students.
+ * _strdup - Duplicate a string
  * @str: char pointer to be determined.
- * Return: char.
+ * Return: The array copied.
  */
 
 char *_strdup(char *str)
@@ -48,4 +48,45 @@ char *_strdup(char *str)
 		array[i++] = '\0';
 	}
 	return (array);
+}
+
+/**
+ * new_dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: third member.
+ * Return: struct.
+ * Description: Longer description
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *new_name, *new_owner;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_name = _strdup(name);
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_owner = _strdup(owner);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
+	return (new_dog);
 }
