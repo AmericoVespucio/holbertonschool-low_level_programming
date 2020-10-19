0f3a22e073d83586178bd368e5c670da5028cf1d
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-19 21:31:34 +0000

    Print Dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print the name of a program
+ * @d: member
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL) /* If d is NULL print nothing. */
+	{
+		return;
+	}
+	if (d->name == NULL) /* if name is NULL, print Name: (nil) */
+	{
+		printf("Name: (nil)\n");
+	}
+	else /* If name is not NULL, print name value */
+	{
+		printf("Name: %s\n", d->name);
+	/* Also print age, since doesnt have any conditions to print it */
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL) /* If an element of d is NULL */
+	{
+		printf("Owner: (nil)\n");
+	}
+	else /* If its not NULL, then print owner name */
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
