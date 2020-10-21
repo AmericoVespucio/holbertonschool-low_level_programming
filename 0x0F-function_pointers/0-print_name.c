80ff920803ec7f5faab611d598d2fe28d036e2e9
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-21 19:25:42 +0000

    Commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,9 @@
  * @f: function pointer
  * Return: Nothing.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL) /* If function DOES NOT enters at null or has null value */
-{
-	(*f)(name);; /* Then call the function with parameter name */
-}
+	if (f != NULL)
+		(*f)(name);
 }
