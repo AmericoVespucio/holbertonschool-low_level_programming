d728433531de1629c00d0478134df0f4c1fdfc2e
AmericoVespucio <estebanaguiarx@gmail.com>
2020-10-27 01:09:44 +0000

    p

--- proyecto/adv_func.c
+++ proyecto/adv_func.c
@@ -10,23 +10,28 @@
  */
 int print_uint(va_list list)
 {
-	unsigned int i, p, r;
-	unsigned int n = va_arg(list, int), count = 0;
 
-	i = n;
-	p = 1;
-	r = i;
+	unsigned int num, div, i = 0;
 
-	while (r > 9)
+	num = va_arg(list, int);
+
+	if (num == 0)
 	{
-		p *= 10;
-		r /= 10;
+		_putchar('0');
+		i = 1;
 	}
-
-	for (; p >= 1; p /= 10)
+	if (num > 0)
 	{
-		_putchar(((i / p) % 10) + '0');
-		count++;
+		for (div = 1; (num / div) > 9; div *= 10)
+			;
+		while (div != 0)
+		{
+			_putchar((num / div) + '0');
+			num %= div;
+			div /= 10;
+			i++;
+		}
 	}
-	return (count);
+	return (i);
+
 }
