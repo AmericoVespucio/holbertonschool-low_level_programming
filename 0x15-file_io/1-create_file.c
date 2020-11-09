25502715a362884f98db3f76c396ab6339f26f4c
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-11-09 19:22:51 -0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,33 @@
 #include "holberton.h"
 
 /**
- * create_file - Function that creates a file.
- * @filename: The name of the file to create.
- * @text_content: Is a string to write to the file.
- * Return: 1 on success, -1 on failure.
- */
+* create_file - Function that creates a file
+*@filename: name of the file to create
+*@text_content: text to write in the file
+*Return: 1 in succes, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int i;
+	int file, retval, count = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-	if (fd < 0)
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		if (errno == EEXIST)
+		while (text_content[count] != '\0')
 		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
-			if (fd == -1)
-				return (-1);
+			count++;
 		}
-		else
-			return (-1);
+		retval = write(file, text_content, count);
 	}
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		if (write(fd, &text_content[i], 1) == -1)
-			return (-1);
-	}
-	close(fd);
+
+	if (retval == -1 || retval != count)
+		return (-1);
+
 	return (1);
 }
