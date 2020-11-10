e0bac40b2a0027a516817ec3878936acc03be630
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-11-09 23:06:36 -0300

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,90 +1,84 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "holberton.h"
 
 /**
-* print_error_close - prints the error when close
-*@file: name of the file
-*Return: Nothing
-*/
-
-void print_error_close(char *file)
+ * _strlen - find length of string
+ * @str: string to be evaluated
+ *
+ * Return: length of string
+ */
+int _strlen(char *str)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file);
-	exit(100);
-}
+	int i = 0;
 
-/**
-* print_error_read - prints the error when read
-*@file: name of the file
-*Return: Nothing
-*/
-
-void print_error_read(char *file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-	exit(98);
-}
-
-/**
-* print_error_write - prints the error when write
-*@file: name of the file
-*Return: Nothing
-*/
-
-void print_error_write(char *file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-	exit(99);
-}
-
-/**
-* main - Program that copies the content of a file to another file
-*@argc: argument counter
-*@argv: argument vector (names of the files)
-*Return: the following errors:
-* 97 for syntax error
-* 98 for read error
-* 99 for write error
-* 100 for close error
-*/
-
-int main(int argc, char **argv)
-{
-	int file_from = 0, file_to = 0, retvalue = 0;
-	int readvalue = 1024, closevalue = 0;
-	char buff[1024];
-
-	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from < 0)
-		print_error_read(argv[1]);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-		| S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_to < 0)
+	while (*str != '\0')
 	{
-		close(file_from);
-		print_error_write(argv[2]);
+		i++;
+		str++;
 	}
-	while (readvalue == 1024)
-	{
-		readvalue = read(file_from, buff, 1024);
-		if (readvalue < 0)
-			print_error_read(argv[1]);
-		retvalue = write(file_to, buff, readvalue);
-		if (retvalue < readvalue)
-			print_error_write(argv[2]);
-	}
-	closevalue = close(file_from);
-	if (closevalue < 0)
-		print_error_close(argv[1]);
-	closevalue = close(file_to);
-	if (closevalue < 0)
-		print_error_close(argv[2]);
-	return (0);
+
+	return (i);
 }
+
+/**
+ * _err - prints a message to stderr and exits the program
+ * @i: error code
+ * @fn: name of file that caused the error
+ */
+void _err(int i, char *fn)
+{
+	switch (i)
+	{
+	case 97:
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+		break;
+	case 98:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fn);
+		exit(98);
+		break;
+	case 99:
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fn);
+		exit(99);
+		break;
+	case 100:
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", fn);
+		exit(100);
+		break;
+	default:
+		break;
+	}
+}
+
+/**
+ * main - copied the content of a file to another file
+ * @ac: number of elements input from command line
+ * @av: elements from command line
+ *
+ * Return: 0 (Always Success)
+ */
+int main(int ac, char **av)
+{
+	int fdFrom;
+	int fdTo;
+	int r;
+	char buf[1024];
+	char err_buf[] = " ";
+
+	if (ac != 3)
+		_err(97, NULL);
+	fdFrom = open(av[1], O_RDONLY);
+
+	if (fdFrom < 0)
+		_err(98, av[1]);
+
+	fdTo = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fdTo == -1)
+		_err(99, av[2]);
+
+	do {
+		r = read(fdFrom, buf, 1024);
+		if (r < 0)
+			_err(98, av[1]);
+		if (write(fdTo, buf, r) != r)
+			_err(99, av[2]);
